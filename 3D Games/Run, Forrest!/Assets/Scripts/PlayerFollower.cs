d9e7580b16a016f73856d7598011e79f95c7bf5c
Alexander Papirnyk <43519694+Gennerys@users.noreply.github.com>
2019-08-30 02:08:26 +0300

    First game added

---
+++ 3D Games/Run, Forrest!/Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    public Transform playerPosition;
+    public Vector3 offset;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerPosition.position + offset;
+    }
+}
